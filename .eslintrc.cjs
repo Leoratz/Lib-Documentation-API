28dc9828f5bb0ad7b147e38caf8cbe7a73d7df77
Léora Chriqui <leora.chriqui@gmail.com>
2025-07-09 10:44:48 +0200

    Init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
+};
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+console.log("Hello rest-to-openapi");
